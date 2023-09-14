6dd018a040e46ccb657581fed4c0dc940777a940
Nadia Paz <nadina.abouhamdan@gmail.com>
2023-09-14 17:11:34 -0500

    add get target var

--- week2/wrangle.py
+++ week2/wrangle.py
@@ -16,6 +16,7 @@ def split_data(df):
     '''
     splits dataframe into train, validate, test
     where train is 60%, validate - 20%,test - 20% 
+    Returns dataframes: df_train, df_val, df_test
     '''
     # make the split reproducible
     np.random.seed(2912)
@@ -40,4 +41,41 @@ def split_data(df):
     df_test = df_shuffled.iloc[n_train+n_val:].copy().reset_index(drop=True)
 
     return df_train, df_val, df_test
-    
+
+def get_target_vars(df_train, df_val, df_test):
+    ''' 
+    Separates the target variable from train, validate and test data sets.
+    Delets the column with target variable from all 3 data sets.
+    Returns numpy arrays: y_train, y_val, y_tes
+    '''
+    # separate the target variable and make its logarithmic transformation
+    y_train = np.log1p(df_train.msrp.values)
+    y_val = np.log1p(df_val.msrp.values)
+    y_test = np.log1p(df_test.msrp.values)
+
+    # drop the target variable from the data sets
+    del df_train['msrp']
+    del df_val['msrp']
+    del df_test['msrp']
+
+    return y_train, y_val, y_test
+
+def get_target_var(df: pd.DataFrame, target: str):
+    ''' 
+    Parameters:
+        df -> dataframe
+        target -> string, name of the column with the target variable
+    Separates the target variable from a data set.
+    Delets a column with a target variable from it.
+    Returns: 
+        numpy array
+    '''
+    # separate the target variable and make its logarithmic transformation
+    y = np.log1p(df[target].values)
+
+
+    # drop the target variable from the data sets
+    del df[target]
+
+    return y
+        
