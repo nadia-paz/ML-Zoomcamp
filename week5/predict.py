1dc4dea84bdf06d0df720bf67a01c27bbdecb0cc
Nadia Paz <nadina.abouhamdan@gmail.com>
2023-10-09 18:14:08 -0500

    add web service

--- week5/predict.py
+++ week5/predict.py
@@ -1,34 +1,26 @@
 import pickle
+from flask import Flask
+from flask import request
+from flask import jsonify
 
 model_file = 'model_C1.bin'
-customer = {
-    'customerid': '8879-zkjof',
-    'gender': 'female',
-    'seniorcitizen': 0,
-    'partner': 'no',
-    'dependents': 'no',
-    'tenure': 41,
-    'phoneservice': 'yes',
-    'multiplelines': 'no',
-    'internetservice': 'dsl',
-    'onlinesecurity': 'yes',
-    'onlinebackup': 'no',
-    'deviceprotection': 'yes',
-    'techsupport': 'yes',
-    'streamingtv': 'yes',
-    'streamingmovies': 'yes',
-    'contract': 'one_year',
-    'paperlessbilling': 'yes',
-    'paymentmethod': 'bank_transfer_(automatic)',
-    'monthlycharges': 79.85,
-    'totalcharges': 3320.75
-}
+app = Flask('churn')
 
 with open(model_file, 'rb') as f_in:
     dv, model = pickle.load(f_in)
 print('Model loaded')
 
-X = dv.transform([customer])
-y_pred = model.predict_proba(X)[0, 1]
+@app.route('/predict', methods=['POST'])
+def predict():
+    customer = request.get_json()
+    X = dv.transform([customer])
+    y_pred = model.predict_proba(X)[0, 1]
+    churn = y_pred >= 0.5
+    result = {
+        "churn_probability": round(float(y_pred), 2),
+        "churn": bool(churn)
+    }
+    return jsonify(result)
 
-print('Churn probability', y_pred)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=9696)
