df22f874a6a91c6ed76868f6cf9a055ce1464e9e
Nadia Paz <nadina.abouhamdan@gmail.com>
2023-11-26 23:45:46 -0600

    change url

--- week9/test.py
+++ week9/test.py
@@ -1,7 +1,9 @@
 import requests
+import aws_url as au
 
-url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
+#url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
 
+url = au.get_url()
 data = {'url': 'http://bit.ly/mlbookcamp-pants'}
 
 result = requests.post(url, json=data).json()
